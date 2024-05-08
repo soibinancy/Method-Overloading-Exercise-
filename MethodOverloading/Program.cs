612ba440f43ec8d8452b9254385c0488a297a6f3
Soibi Nancy <39niches@gmail.com>
2024-05-08 00:26:34 -0500

    exercise complete on method overload

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var intAdd = Add(2, 2);
+            Console.WriteLine(intAdd);
+
+            var decimalAdd = Add(2.0m, 2.0m);
+
+            var addWithBool = Add(0, 1, false);
+            Console.WriteLine(addWithBool);
+        }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isChecked)
+        {
+            var sum = numOne + numTwo; 
+
+            if(isChecked && sum > 1)
+            {
+                return $"{sum} dollars";      
+            }
+            else if(isChecked && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString(); 
+            }
+
         }
     }
 }
